0f84c909c4c97e9b9e8f8fb98842b86e3700e22e
IT24CVillarosa <drianv5335@gmail.com>
2024-09-23 12:25:05 +0800

    initial commit

---
+++ src/it20_villarosa/IT20_Villarosa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it20_villarosa;
+
+/**
+ *
+ * @author Adrian M. Villarosa
+ */
+public class IT20_Villarosa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("IT20C Project Repository");
+    }
+    
+}
