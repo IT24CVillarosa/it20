154c3076d09f59848986117c33dd1c21e0f38548
IT24CVillarosa <drianv5335@gmail.com>
2024-09-25 11:24:16 +0800

    initial commit

---
+++ src/First_Package/SimpleArray1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package First_Package;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class SimpleArray1 {
+    
+    public static void main(String[] args) {
+       
+        String[] fruits = {"Apple", "Banana", "Cherry", "Grape"};
+
+        
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+        System.out.println(fruits[3]);
+    }
+}
+
+    
+
---
+++ src/Second_Package/SimpleArray2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Second_Package;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class SimpleArray2 {
+    
+    public static void main(String[] args) {
+        
+        String[] fruits = {"Apple", "Banana", "Cherry", "Grape"};
+
+       
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+    }
+}
+
+    
+
